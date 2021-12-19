ad831f0f7f6c7a7c32508e4061f4698a42fd11d9
Green <kirill.31030086@gmail.com>
2021-12-19 19:42:06 +0300

    Added the ability to merge images into one panoramic

---
+++ OpenCV/Panorama/Panorama.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <fstream>
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/stitching.hpp"
+
+using namespace std;
+using namespace cv;
+
+vector<Mat> ReadImages(vector<string> arFilePaths)
+{
+    if (arFilePaths.empty())
+        return vector<Mat>();
+
+    vector<Mat> arImages;
+
+    for (string wsFilePath : arFilePaths)
+    {
+        Mat oImage = imread(wsFilePath);
+        if (!oImage.data)
+        {
+            cout << "Can't read image \"" << wsFilePath << "\"" << endl;
+            return vector<Mat>();
+        }
+
+        arImages.push_back(oImage);
+    }
+
+    return arImages;
+}
+
+int main()
+{
+    Stitcher::Mode enMode = Stitcher::PANORAMA;
+
+    vector<Mat> arImages = ReadImages({ "../Images/panarama1.jpg",
+                                        "../Images/panarama2.jpg",
+                                        "../Images/panarama3.jpg" });
+
+
+    Mat oNewImage;
+
+    Ptr<Stitcher> oStitcher = Stitcher::create(enMode);
+
+    Stitcher::Status enStatus = oStitcher->stitch(arImages, oNewImage);
+
+    if (enStatus != Stitcher::OK)
+    {
+        cout << "Can't stitch images\n";
+        return -1;
+    }
+
+    namedWindow("Glued image", WINDOW_AUTOSIZE);
+    moveWindow("Glued image", 100, 100);
+    imshow("Glued image", oNewImage);
+    waitKey(0);
+
+    return 0;
+}
